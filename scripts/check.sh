5a59c960160402286c941544b394eb62cca8dcf7
uncenter <47499684+uncenter@users.noreply.github.com>
2023-12-29 16:54:33 -0500

    ci(release): add check to make sure nothing exceeds 10mb limit

---
+++ scripts/check.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+cd ./lib/
+
+for dir in */; do
+    cd "$dir"
+    size=$(stat -f%z ./package.json)
+
+    echo "$dir: $size"
+
+    if [ $((size / 1000000)) -gt 10 ]; then
+        echo "ERROR: size of $dir exceeds 10MB!"
+        exit 1
+    fi
+
+    cd ..
+done
+
