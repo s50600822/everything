2bc78df1937ab17ea59cb18452b41e5afaa09d45
uncenter <47499684+uncenter@users.noreply.github.com>
2023-12-29 15:55:20 -0500

    chore: update eslint/prettier configs, format with prettier

--- src/build.js
+++ src/build.js
@@ -1,19 +1,19 @@
-const names = require("all-the-package-names");
-const fs = require("fs");
-const path = require("path");
+const names = require('all-the-package-names');
+const fs = require('fs');
+const path = require('path');
 
-const { getFirstChar, getPkgJsonData } = require("./utils");
+const { getFirstChar, getPkgJsonData } = require('./utils');
 
 const scopes = [
-	..."abcdefghijklmnopqrstuvwxyz".split(""),
-	..."1234567890".split(""),
+	...'abcdefghijklmnopqrstuvwxyz'.split(''),
+	...'1234567890'.split(''),
 ];
 
 const packages = {};
 
 for (const name of names) {
 	const firstChar = getFirstChar(name);
-	const scope = scopes.includes(firstChar) ? firstChar : "other";
+	const scope = scopes.includes(firstChar) ? firstChar : 'other';
 
 	if (!(scope in packages)) {
 		packages[scope] = [];
@@ -22,7 +22,7 @@ for (const name of names) {
 	packages[scope].push(name);
 }
 
-const LIB = path.join(__dirname, "../lib/");
+const LIB = path.join(__dirname, '../lib/');
 if (fs.existsSync(LIB)) fs.rmSync(LIB, { recursive: true });
 
 for (const [scope, dependencies] of Object.entries(packages)) {
@@ -33,24 +33,24 @@ for (const [scope, dependencies] of Object.entries(packages)) {
 		name: `@everything-registry/${scope}`,
 		...getPkgJsonData(),
 		dependencies: dependencies.reduce((acc, curr) => {
-			acc[curr] = "*";
+			acc[curr] = '*';
 			return acc;
 		}, {}),
 	});
 
-	fs.writeFileSync(path.join(dir, "package.json"), pkgJson);
-	fs.writeFileSync(path.join(dir, "index.js"), "console.log('Beep boop!');");
+	fs.writeFileSync(path.join(dir, 'package.json'), pkgJson);
+	fs.writeFileSync(path.join(dir, 'index.js'), "console.log('Beep boop!');");
 }
 
-const dir = path.join(LIB, "everything");
+const dir = path.join(LIB, 'everything');
 fs.mkdirSync(dir, { recursive: true });
 const pkgJson = JSON.stringify({
 	name: `everything`,
 	...getPkgJsonData(),
 	dependencies: Object.keys(packages).reduce((acc, curr) => {
-		acc[`@everything-registry/${curr}`] = require("../package.json").version;
+		acc[`@everything-registry/${curr}`] = require('../package.json').version;
 		return acc;
 	}, {}),
 });
-fs.writeFileSync(path.join(dir, "package.json"), pkgJson);
-fs.writeFileSync(path.join(dir, "index.js"), "console.log('Beep boop!');");
+fs.writeFileSync(path.join(dir, 'package.json'), pkgJson);
+fs.writeFileSync(path.join(dir, 'index.js'), "console.log('Beep boop!');");
--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,23 @@
 function getFirstChar(name) {
 	const initialChar = name.charAt(0).toLowerCase();
 
-	return initialChar === "@"
-		? name.split("/")[1].charAt(0).toLowerCase()
+	return initialChar === '@'
+		? name.split('/')[1].charAt(0).toLowerCase()
 		: initialChar;
 }
 
 function getPkgJsonData() {
 	return {
-		version: require("../package.json").version,
-		description: "npm install everything",
-		main: "index.js",
+		version: require('../package.json').version,
+		description: 'npm install everything',
+		main: 'index.js',
 		contributors: [
-			"PatrickJS <github@patrickjs.com>",
-			"uncenter <hi@uncenter.dev>",
+			'PatrickJS <github@patrickjs.com>',
+			'uncenter <hi@uncenter.dev>',
 		],
-		keywords: ["everything", "allthethings", "everymodule"],
-		license: "MIT",
-		homepage: "https://github.com/everything-registry/everything",
+		keywords: ['everything', 'allthethings', 'everymodule'],
+		license: 'MIT',
+		homepage: 'https://github.com/everything-registry/everything',
 	};
 }
 
