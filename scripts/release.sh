52e638bed40f095e4536b5ce197db4f7e8e086d3
PatrickJS <github@patrickjs.com>
2023-12-29 16:21:01 -0800

    chore: update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env sh
 
-cd ./lib/
+# Navigate to the 'lib' directory or exit if the directory doesn't exist.
+cd ./lib/ || exit
 
+# For each sub-directory of 'lib' represented by 'dir'
 for dir in */; do
-    cd "$dir"
-    npm publish --access public
+    # Navigate into 'dir' or continue to the next 'dir' if this fails
+    cd "$dir" || continue
+
+    # Print the name of the directory we're attempting to publish
+    echo "Publishing directory $dir"
+
+    # Try to publish the npm package publicly. 
+    # If successful, print a success message; if not, print a failure message.
+    if npm publish --access public; then
+        echo "Successfully published $dir"
+    else
+        echo "Failed to publish $dir"
+    fi
+
+    # Navigate back to the parent ('lib') directory
     cd ..
 done
