ef50d279602166a412b79efb567a4e81c7f0b66f
PatrickJS <github@patrickjs.com>
2023-12-29 16:03:33 -0800

    refactor: update file size log

--- src/build.js
+++ src/build.js
@@ -69,11 +69,36 @@ for (const [scope, dependencies] of Object.entries(packages)) {
     },
     PRETTY_PRINT_PACKAGE_JSON,
   );
+  const packageFile = path.join(packageDir, 'package.json');
+  fs.writeFileSync(packageFile, pkgJson);
 
-  fs.writeFileSync(path.join(packageDir, 'package.json'), pkgJson);
+  // grab package.json file size
+  const stats = fs.statSync(packageFile);
+  // size in bytes
+  const fileSizeInBytes = stats.size;
+  // Convert it to megabytes (MB)
+  const fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+  let fileSizeToDisplay;
+
+  if (fileSizeInMegabytes < 1) {
+    // round to 6 decimal places for files < 1MB
+    fileSizeToDisplay = Math.round(fileSizeInMegabytes * 1e6) / 1e6;
+  } else if (fileSizeInMegabytes < 10) {
+    // round to 2 decimal places for file sizes 1MB-10MB
+    fileSizeToDisplay = Math.round(fileSizeInMegabytes * 100) / 100;
+  } else {
+    // round to whole number for file sizes > 10MB
+    fileSizeToDisplay = Math.round(fileSizeInMegabytes);
+  }
+  const msg = `package ${packageName} has the size of ${fileSizeToDisplay}mb`;
+  if (fileSizeInMegabytes > 10) {
+    console.warn(`WARNING: ${packageName} is larger than 10mb!`);
+  }
+
+  console.log(msg);
   fs.writeFileSync(
     path.join(packageDir, 'index.js'),
-    `console.log('Beep boop!');`,
+    `console.log('Beep boop! ${msg}');`,
   );
 }
 
@@ -89,7 +114,6 @@ const everythingPkgJson = stringify(
     repository: {
       type: 'git',
       url: 'https://github.com/everything-registry/everything.git',
-      directory: `lib/${packageName}`,
     },
     dependencies: Object.keys(packages).reduce((acc, curr) => {
       acc[`@everything-registry/${curr}`] =
@@ -99,10 +123,32 @@ const everythingPkgJson = stringify(
   },
   PRETTY_PRINT_PACKAGE_JSON,
 );
-fs.writeFileSync(
-  path.join(everythingPackageDir, 'package.json'),
-  everythingPkgJson,
-);
+const packageFile = path.join(everythingPackageDir, 'package.json');
+fs.writeFileSync(packageFile, everythingPkgJson);
+// grab package.json file size
+const stats = fs.statSync(packageFile);
+// size in bytes
+const fileSizeInBytes = stats.size;
+// Convert it to megabytes (MB)
+const fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+let fileSizeToDisplay;
+
+if (fileSizeInMegabytes < 1) {
+  // round to 6 decimal places for files < 1MB
+  fileSizeToDisplay = Math.round(fileSizeInMegabytes * 1e6) / 1e6;
+} else if (fileSizeInMegabytes < 10) {
+  // round to 2 decimal places for file sizes 1MB-10MB
+  fileSizeToDisplay = Math.round(fileSizeInMegabytes * 100) / 100;
+} else {
+  // round to whole number for file sizes > 10MB
+  fileSizeToDisplay = Math.round(fileSizeInMegabytes);
+}
+const msg = `package ${packageName} has the size of ${fileSizeToDisplay}mb`;
+if (fileSizeInMegabytes > 10) {
+  console.warn(`WARNING: ${packageName} is larger than 10mb!`);
+}
+
+console.log(msg);
 fs.writeFileSync(
   path.join(everythingPackageDir, 'index.js'),
   "console.log('You have installed everything... but at what cost?');",
